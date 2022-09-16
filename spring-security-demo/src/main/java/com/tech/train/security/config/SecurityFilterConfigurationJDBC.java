7486b77af657b0d109fb8320f8d3828cac63d78b
Yashodha Gangadharappa <yashodha.gangadharappa@microfocus.com>
2022-09-16 15:03:55 +0530

    cleanup

--- spring-security-demo/src/main/java/com/tech/train/security/config/SecurityFilterConfigurationJDBC.java
+++ spring-security-demo/src/main/java/com/tech/train/security/config/SecurityFilterConfigurationJDBC.java
@@ -1,33 +1,21 @@
 package com.tech.train.security.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 
 @EnableWebSecurity
 @Configuration
 public class SecurityFilterConfigurationJDBC {
-
-    /*Autowired
-    public DataSource dataSource;
-
-     */
 
     @Bean
     public DataSource dataSource() {
@@ -51,15 +39,4 @@ public class SecurityFilterConfigurationJDBC {
         return users;
     }
 
-   /* @Bean
-    public UserDetailsManager users(DataSource dataSource) {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-        users.createUser(user);
-        return users;
-    } */
 }
