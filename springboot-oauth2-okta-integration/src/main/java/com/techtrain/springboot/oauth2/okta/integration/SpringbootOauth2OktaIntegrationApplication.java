334c398fc98864dbfb1959c66631d14b494e3871
Yashodha Gangadharappa <yashodha.gangadharappa@microfocus.com>
2023-06-03 11:16:26 +0530

    add initial commit for okta integration

---
+++ springboot-oauth2-okta-integration/src/main/java/com/techtrain/springboot/oauth2/okta/integration/SpringbootOauth2OktaIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.techtrain.springboot.oauth2.okta.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauth2OktaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOauth2OktaIntegrationApplication.class, args);
+	}
+
+}
---
+++ springboot-oauth2-okta-integration/src/test/java/com/techtrain/springboot/oauth2/okta/integration/SpringbootOauth2OktaIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtrain.springboot.oauth2.okta.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauth2OktaIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
